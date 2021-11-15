b1d0d8e276692880cf6778030b12746e5b3e2798
Jade Meskill <jade.meskill@gmail.com>
2021-11-14 23:04:08 -0700

    fix broken logic

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 use std::path::{Path, PathBuf};
 use walkdir::DirEntry;
-
 pub fn tilde_path(path: &Path) -> PathBuf {
     let st = shellexpand::tilde(path.to_str().unwrap()).to_string();
     return Path::new(&st).to_path_buf();
@@ -18,5 +17,5 @@ pub fn is_not_hidden(entry: &DirEntry) -> bool {
 pub fn is_not_excluded(entry: &DirEntry, src_base: &Path, excludes: &HashSet<PathBuf>) -> bool {
     let src = entry.path();
     let src_rel = src.strip_prefix(src_base).unwrap();
-    excludes.contains(src_rel)
+    !excludes.contains(src_rel)
 }
